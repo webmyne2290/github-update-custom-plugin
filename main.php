7504773634d6e972ad244333e71af57747bb6da7
webmyne2290 <68014382+webmyne2290@users.noreply.github.com>
2020-07-15 14:15:44 +0530

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
  * Plugin Name: Github Custom Update Plugin
  * Plugin URI: http://www.webmyne.com/
  * Description: This plugin works as custom plugin create and get update now in admin area
- * Version: 4.0
+ * Version: 5.0
  * Author: Webmyne
  * Author URI: http://www.webmyne.com/
  */
